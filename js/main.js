e96397dcd987d751b25cfffc237446e8e9ac20dc
Леонид Салтыков <leonidyarmak@gmail.com>
2018-04-03 03:17:58 +0300

    fix styles

--- js/main.js
+++ js/main.js
@@ -24,41 +24,45 @@ function showTasks(arr, first){
 
     let i = (first) ? 0 : arr.length - 1;
 
-    if(!i){
-        console.log('del')
-        listTasks.innerHTML = '';
-    }
-    
+    if(!i)
+    	listTasks.innerHTML = '';
+        
     for(; i < arr.length; i++){
-        if (!arr[i].remove) {
-            
-        let buttonDel  = document.createElement('button'),
-            buttonComp = document.createElement('button'),
-            li         = document.createElement('li');
+        if (!arr[i].remove) {            
+        let imgDel  = document.createElement('img'),
+            imgComp = document.createElement('img'),
+            li      = document.createElement('li');
 
-            buttonDel.textContent = 'Del';
-            buttonDel.classList.add('button-del');
-            buttonDel.addEventListener('click', removeTask);
-            buttonDel.dataset.index = i;
+            imgDel.classList.add('img-del');
+            imgDel.addEventListener('click', removeTask);
+            imgDel.dataset.index = i;
+            imgDel.src = './img/delete.png';
             
-            buttonComp.textContent = 'Ok';
-            buttonComp.classList.add('button-complete');
-            buttonComp.addEventListener('click', completeTask);
-            buttonComp.dataset.index = i;
+            imgComp.classList.add('img-complete');
+            imgComp.addEventListener('click', completeTask);
+            imgComp.dataset.index = i;
+            imgComp.src = './img/check.png';
             
-            (!arr[i].complete) ? li.classList.add('list-item') : li.classList.add('list-item-complete')     
+            if(!arr[i].complete) {
+            	li.classList.add('list-item');
+            	imgComp.src = './img/uncheck.png'; 
+            	
+            }else {
+            	li.classList.add('list-item-complete');
+            	imgComp.src = './img/check.png'
+            }
             
             li.textContent = arr[i].val;
-            li.appendChild(buttonComp);
-            li.appendChild(buttonDel);                
+            li.appendChild(imgComp);
+            li.appendChild(imgDel);                
             listTasks.appendChild(li);
-            }
+        }
     }   
 }
 
 function removeTask(event){
     let i = event.target.dataset.index;
-    console.log(event.target, i, tasksArr, listTasks.children[i])
+    // console.log(event.target, i, tasksArr, listTasks.children[i])
 
     if (tasksArr[i]){
         tasksArr[i].remove = true;
@@ -77,8 +81,10 @@ function completeTask(event){
 
     if(!status){
         listTasks.children[i].classList.replace('list-item', 'list-item-complete');
+        event.target.src = './img/check.png'
     } else{
         listTasks.children[i].classList.replace('list-item-complete', 'list-item');
+        event.target.src = './img/uncheck.png'
     }
 }
 
@@ -90,18 +96,50 @@ function showStats (elem){
 function removeAll(){
 
     let res = tasksArr.filter((item, i)=>{
-        console.log(i)
         return !item.complete
     })
 
     tasksArr.length = 0;
     tasksArr = [...res];
-    showTasks(tasksArr, true);
+    showTasks(res, true);
 }
 
-function completeAll(){
+let check = false;
+
+function completeAll(event){	
     for (let i = 0; i < tasksArr.length; i++) {
         tasksArr[i].complete = !tasksArr[i].complete;
     }
+    if(check && tasksArr.length){
+    	event.target.src = './img/complete-all.png';
+    	check = !check
+    } else if(tasksArr.length){
+    	event.target.src = './img/check-complete-all.png'
+    	check = !check
+    }
     showTasks(tasksArr, true);
+}
+
+function sortTasks(event){
+	
+	let items = document.querySelectorAll('.item-sorter-active');
+	if(event.target.tagName ==='SPAN'){
+
+	for(let i = 0; i < items.length; i++){
+		items[i].className = ('item-sorter')
+	}
+		event.target.className = ('item-sorter-active')
+	}
+	let result = tasksArr.filter((item, i)=>{
+        if(event.target.textContent === 'All'){
+        	return item 
+        }
+        else if(event.target.textContent === 'Completed'){
+        	return (item.complete) ? true : false
+        }
+        else if(event.target.textContent === 'Active'){        	
+        	return (!item.complete) ? true : false
+        }
+    })
+    showTasks(result, true);	
 }
